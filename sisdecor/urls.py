02119fcb806efbf9277347e35c140dcdd9d2e72e
geraldogrj <geraldogrj@gmail.com>
2022-08-18 21:35:28 -0300

    criado o app Clientes

--- sisdecor/urls.py
+++ sisdecor/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from calendario.views import CalendarView
+from clientes.views import HomeClientes
 from home.views import HomeSisDecor
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeSisDecor.as_view(), name='home'),
     path('calendario', CalendarView.as_view(), name='calendario'),
+    path('clientes', HomeClientes.as_view(), name='clientes'),
 ]
